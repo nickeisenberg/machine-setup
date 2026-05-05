b69334cffaf7d69eb0f6977d0cfdae7368dfd3a0
nick <nick@localhost.localdomain>
2026-05-05 18:19:31 -0400

    typo fix in start here run1. makeing a fir for cuda runfile

--- nvidia-cuda-setup/cuda-12_9.sh
+++ nvidia-cuda-setup/cuda-12_9.sh
@@ -1,4 +1,7 @@
 sudo dnf install gcc
 
+mkdir ${HOME}/software/cuda
+cd ${HOME}/software/cuda
+
 wget https://developer.download.nvidia.com/compute/cuda/12.9.0/local_installers/cuda_12.9.0_575.51.03_linux.run
 sudo sh cuda_12.9.0_575.51.03_linux.run
--- start-here/run1.sh
+++ start-here/run1.sh
@@ -4,5 +4,8 @@ subscription-manager repos --enable=rhel-9-for-$arch-appstream-rpms
 subscription-manager repos --enable=rhel-9-for-$arch-baseos-rpms
 subscription-manager repos --enable=codeready-builder-for-rhel-9-$arch-rpms
 
-sudo dnf install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.noarch.rpms
+sudo dnf install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.noarch.rpm
 
+sudo dnf install -y \
+	gcc \
+	xclip
