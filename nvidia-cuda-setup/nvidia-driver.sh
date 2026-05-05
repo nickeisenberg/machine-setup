a874f751ab940ee894b516a57ba9ccd8b87f4af9
nick <nick@localhost.localdomain>
2026-05-05 16:37:22 -0400

    updates to the nvida driver install script

--- nvidia-cuda-setup/nvidia-driver.sh
+++ nvidia-cuda-setup/nvidia-driver.sh
@@ -1,24 +1,18 @@
 # See https://docs.nvidia.com/datacenter/tesla/driver-installation-guide/red-hat-enterprise-linux.html
-# for more detail. Nvidia documentation is some of the worst I have ever read
-# and the above link is only 95% correct. Below is the dubugged version of the
-# above link.
+# for more detail.
 
 arch=x86_64
 distro=rhel9
-driver_version=latest
+driver_version=580-open
 
 subscription-manager repos --enable=rhel-9-for-$arch-appstream-rpms
 subscription-manager repos --enable=rhel-9-for-$arch-baseos-rpms
 subscription-manager repos --enable=codeready-builder-for-rhel-9-$arch-rpms
-
 sudo dnf install https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.noarch.rpm
-
 sudo dnf config-manager --add-repo https://developer.download.nvidia.com/compute/cuda/repos/$distro/$arch/cuda-$distro.repo
 
-sudo dnf clean expire-cache
-
 sudo dnf module enable nvidia-driver:${driver_version}
-sudo dnf install nvidia-driver
-sudo dnf install nvidia-driver-cuda
 
-# sudo reboot
+sudo dnf install nvidia-open
+
+sudo reboot
