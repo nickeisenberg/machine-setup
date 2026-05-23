5b357740da1dbfcbeeff9037836b2817002a91e4
nick <nick@localhost.localdomain>
2026-05-23 17:33:34 -0400

    nginx tested. seems to work

--- llm-serving/tmp/nginx-setup/get-nginx.sh
+++
@@ -1,1 +0,0 @@
-podman pull docker.io/library/nginx:latest
--- llm-serving/tmp/nginx-setup/run-nginx.sh
+++
@@ -1,8 +0,0 @@
-podman run \
-    --name nginx \
-    --network llm-net \
-    --replace \
-    -p 8001:8001 \
-    -v $(pwd)/nginx/nginx.conf:/etc/nginx/nginx.conf:ro,Z \
-    -v $(pwd)/nginx/tokens.map:/etc/nginx/tokens.map:ro,Z \
-    docker.io/library/nginx:latest
