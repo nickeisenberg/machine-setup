df7f502b46450c35777aa13e00d5c67a5e00a5b8
nicholas <nicholas@localhost.localdomain>
2026-05-26 17:32:06 -0400

    cuda install fix

--- nvidia-cuda-setup/cuda-12_9.sh
+++ nvidia-cuda-setup/cuda-12_9.sh
@@ -1,6 +1,6 @@
 sudo dnf install gcc
 
-mkdir ${HOME}/software/cuda
+mkdir -p ${HOME}/software/cuda
 cd ${HOME}/software/cuda
 
 wget https://developer.download.nvidia.com/compute/cuda/12.9.0/local_installers/cuda_12.9.0_575.51.03_linux.run
