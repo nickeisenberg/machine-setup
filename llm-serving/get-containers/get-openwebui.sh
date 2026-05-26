6a73cbc8a2738c8644542fc3e36ae8efb2b5a632
eisenbnt <eisenbnt@nv.doe.gov>
2026-05-26 09:29:34 -0700

    WIP

--- llm-serving/get-containers/get-openwebui.sh
+++ llm-serving/get-containers/get-openwebui.sh
@@ -1,2 +1,2 @@
-sudo podman pull ghcr.io/open-webui/open-webui:main
-sudo podman pull ghcr.io/open-webui/pipelines:main
+podman pull ghcr.io/open-webui/open-webui:main
+podman pull ghcr.io/open-webui/pipelines:main
