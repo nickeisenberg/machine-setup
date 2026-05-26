a3a06a8bb4d1de6052c486d3a7809fe81552131e
nicholas <nicholas@localhost.localdomain>
2026-05-26 16:40:58 -0400

    nvidia driver now dkms install

--- nvidia-cuda-setup/nvidia-driver.sh
+++ nvidia-cuda-setup/nvidia-driver.sh
@@ -3,16 +3,18 @@
 
 arch=x86_64
 distro=rhel9
-driver_version=580-open
+# driver_version=580-open
+driver_version=580-dkms
 
 subscription-manager repos --enable=rhel-9-for-$arch-appstream-rpms
 subscription-manager repos --enable=rhel-9-for-$arch-baseos-rpms
 subscription-manager repos --enable=codeready-builder-for-rhel-9-$arch-rpms
 sudo dnf install https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.noarch.rpm
+
 sudo dnf config-manager --add-repo https://developer.download.nvidia.com/compute/cuda/repos/$distro/$arch/cuda-$distro.repo
 
 sudo dnf module enable nvidia-driver:${driver_version}
 
-sudo dnf install nvidia-open
+sudo dnf install -y cuda-drivers
 
-sudo reboot
+# sudo rebootk
