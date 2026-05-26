d77cbe1c72eecdf881cb05b533ddea6d965a9520
nicholas <nicholas@localhost.localdomain>
2026-05-26 17:32:06 -0400

    cuda install fix

--- llm-serving/get-models/get-gemma2.sh
+++ llm-serving/get-models/get-gemma2.sh
@@ -1,7 +1,8 @@
 LLNL_USER=eisenbnt
 SAVE_TO=${HOME}/models
+MODEL="gemma-3-270m-it"
 
 mkdir -p $SAVE_TO
 
-rsync -avP ${LLNL_USER}@matrix.llnl.gov:/p/lustre1/nova/llm-weights/ \
+scp -r ${LLNL_USER}@matrix.llnl.gov:/p/lustre1/nova/llm-weights/${MODEL} \
     ${SAVE_TO}/
