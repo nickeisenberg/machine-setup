6e480a189c368ac749f1c03fd422c296e106b4f5
nicholas <nicholas@localhost.localdomain>
2026-05-26 18:53:03 -0400

    added some ldap stuff

---
+++ llm-serving/get-containers/get-openldap.sh
@@ -0,0 +1,2 @@
+podman pull docker.io/osixia/openldap:latest
+podman pull docker.io/osixia/phpldapadmin:latest
--- start-here/run1.sh
+++ start-here/run1.sh
@@ -8,6 +8,7 @@ sudo dnf install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.
 
 sudo dnf install -y \
 	gcc \
+	openldap-clients \
 	podman \
 	podman-compose \
 	tmux \
