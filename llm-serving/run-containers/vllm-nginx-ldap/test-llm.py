612473619217b06757332b608503e6b2d8ba5513
nicholas <nicholas@localhost.localdomain>
2026-05-26 19:09:58 -0400

    did some TLS stuff

--- llm-serving/run-containers/vllm-nginx-ldap/test-llm.py
+++ llm-serving/run-containers/vllm-nginx-ldap/test-llm.py
@@ -1,8 +1,10 @@
+import httpx
 from openai import OpenAI
 
 client = OpenAI(
-    base_url="http://localhost:8001/v1",
+    base_url="https://localhost:8001/v1",
     api_key="sk-5183fa987acb5e0950282ef7082cb84a4920bbaf4df984a36c6735f12775c53d",
+    http_client=httpx.Client(verify=False),
 )
 
 response = client.chat.completions.create(
