1f31d35e0f2c9d92a8bced003e3e67594d1489d5
nicholas <nicholas@localhost.localdomain>
2026-06-03 16:35:49 -0400

    openwebui and keycloak models work in UI now.

--- llm-serving/run-containers/openwebui-with-keycloak/run.sh
+++ llm-serving/run-containers/openwebui-with-keycloak/run.sh
@@ -1,3 +1,16 @@
 export LLM_WEIGHTS_DIR="${HOME}/llm-weights"
+export LLM_MODEL_NAME="gemma-3-270m-it"
+
+mkdir -p \
+    ./mnt/keycloak/data \
+    ./mnt/ldap/config \
+    ./mnt/ldap/data \
+    ./mnt/openwebui/data
+
+if [ ! -d "${LLM_WEIGHTS_DIR}/${LLM_MODEL_NAME}" ]; then
+    echo "ERROR: Model directory not found:"
+    echo "  ${LLM_WEIGHTS_DIR}/${LLM_MODEL_NAME}"
+    exit 1
+fi
 
 podman compose up --build
