1c2e86c7b4515d7eaabe5444287ba4e59a339eb7
nicholas <nicholas@localhost.localdomain>
2026-06-03 22:39:51 -0400

    rm dir that should not have been in main

--- llm-serving/run-containers/main/vllm-with-sso/ldap/add-nick.sh
+++
@@ -1,5 +0,0 @@
-ldapadd -x \
-  -H ldap://localhost:1389 \
-  -D "cn=admin,dc=company,dc=local" \
-  -w adminpassword \
-  -f ./nick.ldif
--- llm-serving/run-containers/main/vllm-with-sso/tests/ldap-command.sh
+++
@@ -1,5 +0,0 @@
-ldapsearch -x \
-  -H ldap://localhost:1389 \
-  -D "cn=admin,dc=company,dc=local" \
-  -w adminpassword \
-  -b "dc=company,dc=local"
